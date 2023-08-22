808a3f6aef11239ea2b29a37e4e0e7edf142b27d
yuni-hutsuka <yuni.wille999@gmail.com>
2023-08-23 05:13:14 +0900

    :sparkles: feat: とりあえずポストを投稿するところまではできそう

--- src/components/ClassPost.ts
+++ src/components/ClassPost.ts
@@ -1,9 +1,11 @@
-class Post {
+export class Post {
+  id: number;
   isRoot: boolean;
   content: string;
   createAt: string;
 
   constructor(isRoot: boolean, content: string) {
+    this.id = 0;
     this.isRoot = isRoot;
     this.content = content;
     this.createAt = new Date().toISOString() 
