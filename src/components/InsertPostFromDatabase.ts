b9e8dccbaccd4f8e2e0f4d587c72cca87deba2f8
yuni-hutsuka <yuni.wille999@gmail.com>
2023-08-23 03:41:22 +0900

    :wastebasket: rm: 削除

--- src/components/InsertPostFromDatabase.ts
+++
@@ -1,8 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-
-const supabase = createServerComponentClient({ cookies });
-
-export function insertPostToDatabase(title: string, content: string) {
-  supabase.from("post").insert({ title, content })
-}
