94f900d7cf513ea93216b24a5222c2c0762a79d1
sengoku1 <chiba4729@gmail.com>
2023-08-23 09:44:30 +0900

    feat:ポストクラスの追加

---
+++ src/components/Post.tsx
@@ -0,0 +1,10 @@
+class Topic{
+  id:number;
+  title:string;
+  abstract:string;
+constructor(id:number,title:string,abstract:string){
+this.id = id;
+this.title =title;
+this.abstract=abstract;
+}
+}
