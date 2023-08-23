d5e828088954671db3cb6629aefe6dc2ba92a0f2
shinohara <taisei.shinohara1329@gmail.com>
2023-08-23 11:45:49 +0900

    feat：返信フォームの作成

---
+++ src/components/ReplyBox.tsx
@@ -0,0 +1,38 @@
+import { useForm } from 'react-hook-form'
+import AccountTextarea from '~/app/components/AccountTextarea'
+
+export type ProfileFormValues = {
+  reply_content: string | null
+}
+
+const ReplyBox = () => { 
+  const { register, handleSubmit, formState, setValue, getFieldState, getValues } =
+    useForm<ProfileFormValues>()
+
+  return (
+    <form onSubmit={handleSubmit(() => console.log(getValues() ))}>
+      <AccountTextarea
+        label='返信欄'
+        name='profile'
+        // helpText='必須'
+        register={register}
+        errors={formState.errors}
+        // placeholder="test test"
+        options={{
+          required: '入力してください',
+          maxLength: {
+            value: 3000,
+            message: '文字数が多すぎます',
+          },
+        }}
+      />
+      <button
+        type='submit'
+      >
+        返信する
+      </button>
+    </form>
+  )
+}
+
+export default ReplyBox
