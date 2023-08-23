383085053a631e97363be7e418fc124686cc109c
izumiki <izumiki514@gmail.om>
2023-08-23 11:45:24 +0900

    feat: フォント周りの設定(未完)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,50 @@
 import { NextPage } from 'next';
-import PostCard from './components/PostCard';
+import PostCard from '../components/PostCard';
 
 const Home: NextPage = async () => {
   // const supabase = createServerComponentClient({ cookies });
   // const { data: post } = await supabase.from('post').select();
   // console.log(post);
 
-  return <div className="flex flex-row flex-wrap bg-slate-300 gap-8">
-    <PostCard title={'タイトル'} content={'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'} color={'yuzu'}/>
-    <PostCard title={'タイトル'} content={'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'} color={'mandarin'}/>
-    <PostCard title={'タイトル'} content={'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'} color={'lime'}/>
-    <PostCard title={'タイトル'} content={'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'} color={'grapefruit'}/>
-    <PostCard title={'タイトル'} content={'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'} color={'lemon'}/>
-  </div>;
+  return (
+    <div className="flex flex-row flex-wrap gap-8 bg-slate-200">
+      <PostCard
+        title={'タイトル'}
+        content={
+          'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
+        }
+        color={'yuzu'}
+      />
+      <PostCard
+        title={'タイトル'}
+        content={
+          'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
+        }
+        color={'mandarin'}
+      />
+      <PostCard
+        title={'タイトル'}
+        content={
+          'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
+        }
+        color={'lime'}
+      />
+      <PostCard
+        title={'タイトル'}
+        content={
+          'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
+        }
+        color={'grapefruit'}
+      />
+      <PostCard
+        title={'タイトル'}
+        content={
+          'コンテンツあああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
+        }
+        color={'lemon'}
+      />
+    </div>
+  );
 };
 
 export default Home;
