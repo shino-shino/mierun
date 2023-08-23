0acc6c544d8d2f48b3a7556464385f2591bcc985
sengoku1 <chiba4729@gmail.com>
2023-08-23 15:44:32 +0900

    feat:返信一覧表示

--- src/app/ex_shino/page.tsx
+++ src/app/ex_shino/page.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from 'next';
-import ReplyBox from '~/components/ReplyBox';
 import PostDetail from '~/components/PostDetail';
-
+import PostToMap from '~/components/PostToMap';
+import ReplyBox from '~/components/ReplyBox';
+import postData from '~/constants/postData';
 interface PostProps {
   params: {
     topicId: string;
@@ -12,13 +13,26 @@ const Post: NextPage<PostProps> = async ({ params }) => {
   // const supabase = createServerComponentClient({ cookies });
   // const { data: post } = await supabase.from('post').select();
   // console.log(post);
-
+ const map =  PostToMap(postData);
   return (
     <>
       <PostDetail></PostDetail>
       <br></br>
       <h1>仮：あーすればいいよ</h1>
       <ReplyBox></ReplyBox>
+      <div>{postData.map((post) => {
+      return (
+        <li className="post" key={post.id}>
+              <span className="post_text">
+                {post.content}
+              <span className="posted_time">
+                {post.create_at}
+              </span>
+              </span>
+            </li>
+       );
+       })}
+      </div>
     </>
   );
 };
