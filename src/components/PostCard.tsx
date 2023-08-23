03284f072554f636de7449e103abb95ee7d1c492
yuta-nishi <ypmtk437.0209@gmail.com>
2023-08-23 12:10:27 +0900

    feat: スレッド表示の進捗更新

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,27 +1,23 @@
-'use client'
-type PostCardProps = {
-  title: string
-  content: string
-  color: 'yuzu' | 'mandarin' | 'lime' | 'grapefruit' | 'lemon' 
+'use client';
+
+interface PostCardProps {
+  id: number;
+  title: string;
+  content: string;
+  color: 'yuzu' | 'mandarin' | 'lime' | 'grapefruit' | 'lemon';
 }
 
-const PostCard = ({
-  title,
-  content,
-  color
-}: PostCardProps
-) => {
-  const colorClassName = 'bg-slate-500'
-  // const colorClassName = 'bg-' + color
-  const cardClassName = 'flex w-48 h-32 flex-col space-y-1 ' + colorClassName 
+export const PostCard = ({ title, content, color }: PostCardProps) => {
+  // const colorClassName = 'bg-slate-500';
+  const colorClassName = 'bg-' + color;
+  const cardClassName = 'flex w-48 h-32 flex-col space-y-1 ' + colorClassName;
 
   return (
     <div className={cardClassName}>
-      <label className='h-1/4 truncate'>{title}</label>
-      <label className='h-1/2 text-sm bg-slate-70 line-clamp-3'>{content}</label>
+      <label className="h-1/4 truncate">{title}</label>
+      <label className="bg-slate-70 line-clamp-3 h-1/2 text-sm">
+        {content}
+      </label>
     </div>
-  )
-}
-
-
-export default PostCard
+  );
+};
