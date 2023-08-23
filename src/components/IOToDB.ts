df7e3729076383a5dd9178c5d80713d554b9e85d
yuni-hutsuka <yuni.wille999@gmail.com>
2023-08-23 10:37:44 +0900

    :sparkles: feat: とりあえず

--- src/components/IOToDB.ts
+++ src/components/IOToDB.ts
@@ -8,8 +8,11 @@ export async function postToMierun(post: PostValues) {
     const { error } = await supabase.from("post")
       .insert({
         // id: 0,
-        isRoot: true, 
         content: post.content, 
+        isRoot: true, 
+        isChildminder: post.isChildminder,
+        parent_id: post.parentId,
+        child_id: post.childId,
         create_at: new Date().toISOString()
       });
 
@@ -18,43 +21,3 @@ export async function postToMierun(post: PostValues) {
     alert(error)
   }
 };
-
-export async function getListOfPostFromMierun() {
-  try {
-    const { error } = await supabase.rpc("get_list");
-
-    if (error) throw Error;
-  } catch (error) {
-    alert(error)
-  }
-}
-
-export async function getListOfPostWithRangeFromMierun(range: number) {
-  try {
-    const { error } = await supabase.rpc("get_list", {range_num: range});
-
-    if (error) throw Error;
-  } catch (error) {
-    alert(error)
-  }
-}
-
-export async function getLatestPostFromMierun() {
-  try {
-    const { error } = await supabase.rpc("get_latest");
-
-    if (error) throw Error;
-  } catch (error) {
-    alert(error)
-  }
-}
-
-export async function getThePostFromMierun(targetId: number) {
-  try {
-    const { error } = await supabase.rpc("get_latest", {arg_id: targetId});
-
-    if (error) throw Error;
-  } catch (error) {
-    alert(error)
-  }
-}
