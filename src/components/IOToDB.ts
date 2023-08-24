08581f37bfbf542c7428641a89c1202c0ed0138f
yuta-nishi <ypmtk437.0209@gmail.com>
2023-08-24 14:40:35 +0900

    chore: コンパイルエラー回避のため使用していない関数をコメントアウト

--- src/components/IOToDB.ts
+++ src/components/IOToDB.ts
@@ -1,5 +1,4 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useEffect, useState } from 'react';
 import { Database } from '~/types/Database';
 import { PostValues } from './PostCreater';
 
@@ -28,99 +27,131 @@ export async function postToMierun(post: PostValues) {
   }
 }
 
-export async function downloadDatabase(
-  topicID: string,
-): Promise<PostData[] | undefined> {
-  try {
-    const { data: topic, error: topicError } = await supabase
-      .from('topic')
-      .select('list_of_post')
-      .eq('id', topicID)
-      .single();
+// export async function downloadDatabase(
+//   topicID: string,
+// ): Promise<PostData[] | undefined> {
+//   try {
+//     const { data: topic, error: topicError } = await supabase
+//       .from('topic')
+//       .select('list_of_post')
+//       .eq('id', topicID)
+//       .single();
 
-    if (topicError) throw Error;
-    if (!topic) throw Error;
-    const { data: post, error: postError } = await supabase
-      .from('post')
-      .select()
-      .in('id', listOfPost);
+//     if (topicError) throw Error;
+//     if (!topic.list_of_post) throw Error;
 
-    if (postError) throw Error;
+//     const listOfPost = Object.entries(topic.list_of_post);
 
-    if (post) return new Promise((resolve) => resolve(post));
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     const { data: post, error: postError } = await supabase
+//       .from('post')
+//       .select()
+//       .in('id', listOfPost);
 
-export async function insertReplyWithParentId(
-  targetId: number,
-  post: PostValues,
-) {
-  try {
-    // console.log(post)
-    const { error } = await supabase.from('post').insert({
-      // id: 0,
-      content: post.content,
-      is_root: true,
-      is_childminder: post.isChildminder,
-      parent_id: targetId,
-      // child_id: 0,
-      created_at: new Date().toISOString(),
-    });
+//     if (postError) throw Error;
 
-    if (error) throw Error;
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     if (post) return post;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-// なんで皆関数の戻り値の型をガシガシ省略するんですかねぇ
-async function getReplysId(targetId: number) {
-  // parent_idに親のIDが入っている要素を探す
-  // search elements | targetId == parent_id
-  try {
-    // console.log(post)
-    const { data, error } = await supabase
-      .from('post')
-      .select()
-      .eq('parent_id', targetId);
-    if (error) throw Error;
-    if (data) {
-      const [posts, setPosts] = useState<PostData>();
-      const [loading, setLoading] = useState<boolean>(true);
+// export async function insertReplyWithParentId(
+//   targetId: number,
+//   post: PostValues,
+// ) {
+//   try {
+//     // console.log(post)
+//     const { error } = await supabase.from('post').insert({
+//       // id: 0,
+//       content: post.content,
+//       is_root: true,
+//       is_childminder: post.isChildminder,
+//       parent_id: targetId,
+//       // child_id: 0,
+//       created_at: new Date().toISOString(),
+//     });
+//     const { error } = await supabase.from('post').insert({
+//       // id: 0,
+//       content: post.content,
+//       is_root: true,
+//       is_childminder: post.isChildminder,
+//       parent_id: targetId,
+//       // child_id: 0,
+//       created_at: new Date().toISOString(),
+//     });
 
-      useEffect(() => {
-        downloadDatabase('5').then((res) => {
-          setPosts(res);
-          // console.log('posts',posts)
-        });
-        if (posts) setLoading(false);
-      }, [posts]);
-      let num: number = posts.map((post: PostData) => post.id.valueOf());
-      return num;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     if (error) throw Error;
+//   } catch (error) {
+//     console.error(error);
+//     console.error(error);
+//   }
+// }
 
-async function insertChildIdToParentPost(targetId: number) {
-  // targetのchild IDに返信ポストのIDを挿入
-  const replyId: number = getReplysId(targetId);
-  console.log(replyId);
-  // update row with replyId | targetId == parent_id
-  try {
-    const { error } = await supabase
-      .from('post')
-      .update({ child_id: replyId })
-      .match({ parentid: targetId });
-  } catch (error) {
-    console.log(error);
-  }
-}
+// // なんで皆関数の戻り値の型をガシガシ省略するんですかねぇ
+// async function getReplysId(targetId: number) {
+//   // parent_idに親のIDが入っている要素を探す
+//   // search elements | targetId == parent_id
+//   try {
+//     // console.log(post)
+//     const { data, error } = await supabase
+//       .from('post')
+//       .select()
+//       .eq('parent_id', targetId);
+//     const { data, error } = await supabase
+//       .from('post')
+//       .select()
+//       .eq('parent_id', targetId);
+//     if (error) throw Error;
+//     if (data) {
+//       const [posts, setPosts] = useState<PostData>();
+//       const [loading, setLoading] = useState<boolean>(true);
+//       const [posts, setPosts] = useState<PostData>();
+//       const [loading, setLoading] = useState<boolean>(true);
 
-export async function reply(targetId: number, post: PostValues) {
-  insertReplyWithParentId(targetId, post);
-  insertChildIdToParentPost(targetId);
-}
+//       useEffect(() => {
+//         downloadDatabase('5').then((res) => {
+//           setPosts(res);
+//           setPosts(res);
+//           // console.log('posts',posts)
+//         });
+//         if (posts) setLoading(false);
+//       }, [posts]);
+//       let num: number = posts.map((post: PostData) => post.id.valueOf());
+//       };
+//         if (posts) setLoading(false);
+//       }, [posts];
+//       let num: number = posts.map((post: PostData) => post.id.valueOf());
+//       return num;
+//     } catch (error) {
+//     console.error(error);
+//     console.error(error);
+//   }
+// }
+
+// async function insertChildIdToParentPost(targetId: number) {
+//   // targetのchild IDに返信ポストのIDを挿入
+//   const replyId: number = getReplysId(targetId);
+//   console.log(replyId);
+//   console.log(replyId);
+//   // update row with replyId | targetId == parent_id
+//   try {
+//     const { error } = await supabase
+//       .from('post')
+//       .update({ child_id: replyId })
+//       .match({ parentid: targetId });
+//   } catch (error) {
+//     console.log(error);
+//   try {
+//     const { error } = await supabase
+//       .from('post')
+//       .update({ child_id: replyId })
+//       .match({ parentid: targetId });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// export async function reply(targetId: number, post: PostValues) {
+//   insertReplyWithParentId(targetId, post);
+//   insertChildIdToParentPost(targetId);
+// }
